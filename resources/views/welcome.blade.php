6951b4947781abba5d8e74e0d387fccadf289bba
Kevin L Hall <kevin@voicebitz.com>
2016-01-28 16:12:29 +0100

    Change front page to show test video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,11 @@
 Inspire. Be inspired.
 @stop
 @section('header_title')
-Making videos happier & healthier
+Making vids happier & healthier
 @stop
 @section('header_subtitle')
-using sports & technology to build creative learning environments.
+using subtitles & technology.
 @stop
-{{-- @section('calltoaction')
-<a href="#" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
-@stop --}}
 @section('body')
 <?php /*	<script>
  	 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -35,36 +32,11 @@ using sports & technology to build creative learning environments.
 ?>
       <!--   Icon Section   -->
       <div class="row">
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-            <h5 class="center">Improving relationships.</h5>
-
-            <p class="light">We value loyalty.</p>
-			<p class="light">We take time to build, nurture, and reflect over our most important relationships.</p>
+          <div class="col s12 card swipescrub">
+		      <video id="video" class="responsive-video" controls autoplay="1" muted preload="metadata">
+		        <source src="http://pinduin.com/images/waddlehelp.mp4" type="video/mp4">
+		        <track label="horror" kind="subtitles" srclang="en" src="{{ url('/ccmashup/subtitles/romance/romance.vtt') }}" default>
+		      </video>
           </div>
-        </div>
-
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">alarm_on</i></h2>
-            <h5 class="center">Saving time.</h5>
-
-            <p class="light">We value time.</p>
-			<p class="light">We are improving communication one classroom at a time and providing a simple way to grow, learn, and give back to the world, all at the same time.</p>
-         </div>
-        </div>
-
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">mood</i></h2>
-            <h5 class="center">Having the fun times of our life.</h5>
-
-            <p class="light">We value fun and community.</p>
-			<p class="light">We have fun along the way with our family, our friends, and all our loved ones and we take time to become better citizens of our changing communities.</p>
-          </div>
-        </div>
       </div>
-
-
 @stop
