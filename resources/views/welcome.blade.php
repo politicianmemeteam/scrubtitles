ec469d4091279db48581d7dd883520b0873cbe41
Kevin L Hall <kevin@voicebitz.com>
2016-01-28 16:23:27 +0100

    Testing auto deploy and build from Github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@ using subtitles & technology.
           <div class="col s12 card swipescrub">
 		      <video id="video" class="responsive-video" controls autoplay="1" muted preload="metadata">
 		        <source src="http://pinduin.com/images/waddlehelp.mp4" type="video/mp4">
-		        <track label="horror" kind="subtitles" srclang="en" src="{{ url('/ccmashup/subtitles/romance/romance.vtt') }}" default>
+		        <track label="horror" kind="subtitles" srclang="en" src="{{ url('/ccmashup/testcopy.vtt') }}" default>
 		      </video>
           </div>
       </div>
